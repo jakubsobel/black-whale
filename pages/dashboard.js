532af46c197af7e4b99fd1a793bad67e0c60a02f
Jakub Sobel <qbasob@gmail.com>
2024-01-09 16:27:33 +0100

    Create passage login and dashboard

---
+++ pages/dashboard.js
@@ -0,0 +1,7 @@
+export default function Dashboard({ isAuthorized, userID, todos }) {
+  return (
+    <div className="container m-auto text-center bg-slate-600">
+      <p>You&apos;ve logged in!</p>
+    </div>
+  );
+}
